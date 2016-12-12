10be7fe81938334afd0a6d084bfc5e563c6b282a
alex.paclin <alex.paclin@gmail.com>
2016-12-12 23:55:12 +0500

    db context

---
+++ Validation/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Validation.Models
+{
+	public class Book
+	{
+		[HiddenInput(DisplayValue = false)]
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name = "Название")]
+		public string Name { get; set; }
+
+		[Required]
+		[Display(Name = "Автор")]
+		public string Author { get; set; }
+
+		[Required]
+		[Display(Name = "Год")]
+		public int Year { get; set; }
+	}
+}
---
+++ Validation/Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Validation.Models
+{
+	public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+	{
+		protected override void Seed(BookContext context)
+		{
+			context.Books.Add(new Book { Id = 1, Name = "Война и мир", Author = "Л. Толстой", Year = 1991 });
+			context.Books.Add(new Book { Id = 2, Name = "Отцы и дети", Author = "И. Тургенев", Year = 2000 });
+			context.Books.Add(new Book { Id = 3, Name = "Чайка", Author = "А. Чехов", Year = 2016 });
+
+			base.Seed(context);
+		}
+	}
+}
